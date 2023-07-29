ff3d54993aa31527bbee8cef5d1a7c99f89f9da5
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-07-29 16:47:56 +0200

    fix env variables and add apiVersion to sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 
-const projectId = process.env.SANITY_PROJECT_ID ?? '';
-const dataset = process.env.SANITY_DATASET ?? 'production';
-const title = process.env.SANITY_TITLE ?? 'title';
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '';
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
+const title = process.env.NEXT_PUBLIC_SANITY_TITLE || 'title';
+const apiVersion = process.env.NEXT_PUBLIC_API_VERSION || '2023-05-03';
 
 const config = defineConfig({
+  apiVersion,
   basePath: '/admin',
   dataset,
   plugins: [deskTool()],
