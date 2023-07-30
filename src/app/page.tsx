7974972b7e165d805383a6cc9381550286daf145
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-07-30 14:09:56 +0200

    add more style to app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,38 @@ const Home = async () => {
   const projects = await getProjects();
 
   return (
-    <h1>
-      {projects.map((project) => (
-        <div key={project._id}>
-          <p>{project.name}</p>
-          {project.image && (
-            <Image
-              alt={project.name}
-              height={100}
-              src={project.image}
-              width={200}
-            />
-          )}
-        </div>
-      ))}
-    </h1>
+    <main className="content-center">
+      <h1 className="text-3xl uppercase font-extrabold">Hello world</h1>
+      <h1 className="text-3xl uppercase font-extrabold">
+        my name is{' '}
+        <b className="font-black bg-gradient-to-r from-orange-400 via-red-500 to-purple-500 bg-clip-text text-transparent">
+          Allan Aoudji
+        </b>
+      </h1>
+      <h1 className="text-3xl uppercase font-extrabold">
+        I&apos;m a graphic and web designer
+      </h1>
+      <section className="flex gap-3">
+        {projects.map((project) => (
+          <div
+            className="rounded-lg overflow-hidden bg-white"
+            key={project._id}
+          >
+            {project.image && (
+              <Image
+                alt={project.name}
+                height={100}
+                src={project.image}
+                width={200}
+              />
+            )}
+            <div className="p-2 bg-white">
+              <p className="text-center text-black font-bold">{project.name}</p>
+            </div>
+          </div>
+        ))}
+      </section>
+    </main>
   );
 };
 
