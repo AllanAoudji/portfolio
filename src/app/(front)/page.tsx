735caebbbb03153f6bb003317244300d8b8ed9cd
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-08-01 21:58:44 +0200

    increase h1 font size

--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -6,16 +6,18 @@ const Home = async () => {
 
   return (
     <main className="content-center">
-      <h1 className="text-3xl uppercase font-extrabold">Hello world</h1>
-      <h1 className="text-3xl uppercase font-extrabold">
-        my name is{' '}
-        <b className="font-black bg-gradient-to-r from-orange-400 via-red-500 to-purple-500 bg-clip-text text-transparent">
-          Allan Aoudji
-        </b>
-      </h1>
-      <h1 className="text-3xl uppercase font-extrabold">
-        I&apos;m a graphic and web designer
-      </h1>
+      <div className="text-4xl">
+        <h1 className="uppercase font-extrabold">Hello world</h1>
+        <h1 className="uppercase font-extrabold">
+          my name is{' '}
+          <b className="font-black bg-gradient-to-r from-orange-400 via-red-500 to-purple-500 bg-clip-text text-transparent">
+            Allan Aoudji
+          </b>
+        </h1>
+        <h1 className="uppercase font-extrabold">
+          I&apos;m a graphic and web designer
+        </h1>
+      </div>
       <section className="flex gap-3 flex-wrap">
         {projects.map((project) => (
           <div
