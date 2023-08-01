bb94badf56d301af34fd756df0d0fa43d163e72e
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-08-01 21:23:41 +0200

    better back layout
    use sanity metadata export and add some style

--- src/app/(back)/layout.tsx
+++ src/app/(back)/layout.tsx
@@ -1,14 +1,17 @@
 import Link from 'next/link';
 
-export const metadata = {
-  title: 'Default backend NextJs/sanity v3 app',
-};
+export { metadata } from 'next-sanity/studio/metadata';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <body>
-      <header>
-        <Link href="/">Go back to the front</Link>
+      <header className="px-10 py-4 flex flex-row-reverse">
+        <Link
+          href="/"
+          className="px-5 py-1 border-2 rounded-xl bg-white text-black ease-in-out font-bold"
+        >
+          Go back to the front
+        </Link>
       </header>
       <main>{children}</main>
     </body>
