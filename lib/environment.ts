d9e4b9d570e0517f6112d6d9d69442583e9a021d
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-08-02 16:29:51 +0200

    create a lib/environment.ts file to keep environment variables to a minimum

---
+++ lib/environment.ts
@@ -0,0 +1,5 @@
+export const sanityAPIVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03';
+export const sanityDataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
+export const sanityProjectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '';
