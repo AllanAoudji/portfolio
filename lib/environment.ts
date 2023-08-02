820bb8a421d29c39da6dbb31d12431a8c0101b22
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-08-02 17:59:15 +0200

    add sanityUseCdn variable to environment.ts

--- lib/environment.ts
+++ lib/environment.ts
@@ -4,3 +4,4 @@ export const sanityDataset =
   process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
 export const sanityProjectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '';
 export const sanityReadToken = process.env.SANITY_API_READ_TOKEN;
+export const sanityUseCdn = false;
