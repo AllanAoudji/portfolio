350d1281c59dc191325c4ca5c62f3b1d9615d204
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-08-30 20:37:23 +0200

    lighter animated logo

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -9,7 +9,7 @@ function HomeHeader() {
           alt="animated log"
           className="h-auto max-w-5xl px-2 transition-all w-full sm:px-20"
           height="1080"
-          src="/logo-animated.gif"
+          src="/logo-animated-light.gif"
           width="1920"
         />
       </div>
