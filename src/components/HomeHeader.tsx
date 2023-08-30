52e13978ee73dc612dd4d5808eb8515d3ebc1adc
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-08-30 17:58:31 +0200

    smaller logo (agani)

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -7,7 +7,7 @@ function HomeHeader() {
       <div className="absolute flex h-screen items-center justify-center pointer-events-none w-screen">
         <Image
           alt="animated log"
-          className="h-auto max-w-5xl px-2 w-full sm:px-56"
+          className="h-auto max-w-5xl px-2 transition-all w-full sm:px-20"
           height="1080"
           src="/logo-light.gif"
           width="1920"
