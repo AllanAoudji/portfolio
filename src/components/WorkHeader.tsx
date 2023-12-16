f2a60e975deb618d58b4ddf5fb2f22a497dbfa11
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-12-16 19:16:46 +0100

    update logo

--- src/components/WorkHeader.tsx
+++ src/components/WorkHeader.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Container from './Container';
-import Logo from '@/public/logo-fixe-dark.png';
+import Logo from '@/public/logo.png';
 
 function WorkHeader() {
   return (
